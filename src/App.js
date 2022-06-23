43f43694afb44ad0a24536427556b86ae3f52c6e
bhargav penumetsa <bobby.srrp@gmail.com>
2022-06-23 23:41:45 +0530

    added cpu player todo: implement logic to cpu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { Component, useEffect, useState } from "react";
 import Board from "./components/Board";
 import Choice from "./components/Choice";
 
-
-
 function App() {
-  const val = [1,2,3,4,5,6,7,8,9]
-  const [value,setValue] = useState(val)
-  const [choice,setChoice] = useState(['',true])
+  const [value, setValue] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [choice, setChoice] = useState(["", true]);
+  const [cpu,setCpu] = useState(false)
 
-  function handleChoice (x) {
-    console.log(x)
-    setChoice(x)
+  function handleChoice(x) {
+    console.log(x);
+    setChoice(x);
   }
+  function handleplayerip(x) {
+    if (choice[0] != "") {
+      console.log(x);
+      console.log(x);
+      setValue([...x]);
+      let y = choice[0] == "X" ? "O" : "X";
+      setChoice([y, false]);
+      setCpu(!cpu)
+      }
+    }
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      let x1 = value
+      let z = []
+      for (let i=0; i<x1.length; i++){
+        if(typeof(x1[i])=='number'){
+          z.push(i)
+        }
+      }
+      let index = z[Math.floor(Math.random()*z.length)];
+      x1[index] = choice[0]
+      setValue([...x1]);
+      let z1 = choice[0] == "X" ? "O" : "X";
+      setChoice([z1, false]);
+    },2000)
+  },[cpu])
+
   return (
     <div className="App">
-      <Board board = {value}></Board>
+      <Board
+        board={value}
+        playerip={handleplayerip}
+        choiceplayer={choice[0]}
+      ></Board>
       <br></br>
       {choice[1] && <div>please choose X|O </div>}
-      {choice[1] && <Choice click = {handleChoice}></Choice>}
+      {choice[1] && <Choice click={handleChoice}></Choice>}
       <br></br>
-      {!choice[1] && <div>your choice: {choice} start the game</div>}
-      
+      {/* {!choice[1] && <div>your choice: {choice} start the game</div>} */}
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,10 +2,18 @@ import '../assets/css/board.css'
 
 
 function Board(props) {
+  function handlePlayerip (x) {
+    if (props.choiceplayer != ''){
+      const y = props.board
+      y[x.index] = props.choiceplayer[0]
+      console.log('hi',y,props.choiceplayer,x.index)
+      props.playerip(y)
+    }
+  }
     return (
       <div className="Board">
         {props.board.map((item,index)=>(
-            <div className="cell" key={index}><p>{(typeof(item)=='number')?'':item}</p></div>
+            <div onClick = {()=>{handlePlayerip({index})}} key={index} ><p>{(typeof(item)=='number')?'':item}</p></div>
         ))}
       </div>
     );
