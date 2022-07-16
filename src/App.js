1e4becdb5acb0e73c8e30881aad963674df5f1f6
bhargavraju49 <bobby.srrp@gmail.com>
2022-07-16 10:54:36 +0530

    updated logic cpu

--- src/App.js
+++ src/App.js
@@ -6,65 +6,135 @@ import Choice from "./components/Choice";
 function App() {
   const [value, setValue] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const [choice, setChoice] = useState(["", true]);
-  const [cpu,setCpu] = useState(false)
-  const [start,setStart] = useState(false)
-  const [win,setWin] = useState(false)
-  const [cputurn,setCputurn] = useState(false)
+  const [cpu, setCpu] = useState(false);
+  const [start, setStart] = useState(false);
+  const [win, setWin] = useState(false);
+  const [cputurn, setCputurn] = useState(false);
 
-  function checkWin(x){
-    let y = x
-    console.log(y)
-    let k = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]]
-    let win = false
-    for (let i=0; i<k.length; i++){
-        console.log(y[k[i][0]],y[k[i][1]],y[k[i][2]])
-        if (y[k[i][0]]==y[k[i][1]] && y[k[i][1]]==y[k[i][2]]){
-            win = true
-            console.log(y[k[i][0]],y[k[i][1]],y[k[i][2]],'wiiiiin')
-            break
-        }
+  function checkWin(x) {
+    let y = x;
+    console.log(y);
+    let k = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [3, 4, 5],
+      [6, 7, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+    ];
+    let win = false;
+    for (let i = 0; i < k.length; i++) {
+      console.log(y[k[i][0]], y[k[i][1]], y[k[i][2]]);
+      if (y[k[i][0]] == y[k[i][1]] && y[k[i][1]] == y[k[i][2]]) {
+        win = true;
+        console.log(y[k[i][0]], y[k[i][1]], y[k[i][2]], "wiiiiin");
+        break;
       }
-    return win
+    }
+    return win;
   }
 
   function handleChoice(x) {
     console.log(x);
     setChoice(x);
-    setStart(true)
+    setStart(true);
   }
   function handleplayerip(x) {
     if (choice[0] != "") {
       // console.log(x);
       // console.log(x);
       setValue([...x]);
-        if(checkWin([...x])){setWin(true)}
-        else{setCpu(!cpu)}
+      if (checkWin([...x])) {
+        setWin(true);
+      } else {
+        setCpu(!cpu);
+      }
       let y = choice[0] == "X" ? "O" : "X";
       setChoice([y, false]);
-      setCputurn(true)
-      }
+      setCputurn(true);
     }
+  }
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      let x1 = value
-      let z = []
-      for (let i=0; i<x1.length; i++){
-        if(typeof(x1[i])=='number'){
-          z.push(i)
+  useEffect(() => {
+    setTimeout(() => {
+      let x1 = value;
+      let z1 = choice[0] == "X" ? "O" : "X";
+      let z2 = choice[0];
+
+      let k = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [3, 4, 5],
+        [6, 7, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+      ];
+
+      let z = [];
+      for (let i = 0; i < x1.length; i++) {
+        if (typeof x1[i] == "number") {
+          z.push(i);
         }
       }
-      let index = z[Math.floor(Math.random()*z.length)];
-      x1[index] = (choice[0]!='')?choice[0]:x1[index]
-      setValue(x1);
-      let z1 = choice[0] == "X" ? "O" : "X";
-      if (start){
-        setChoice([z1, false]);
-        if(checkWin(x1)){setWin(true)}
+      let index = z[Math.floor(Math.random() * z.length)];
+
+      for (let i = 0; i < k.length; i++) {
+        console.log(
+          z1,
+          z2,
+          x1[k[i][0]],
+          x1[k[i][1]],
+          x1[k[i][2]],
+          "*******************<<<<<<<<<<<<<<<"
+        );
+        if (
+          (x1[k[i][0]] == z2 && x1[k[i][1]] == z2) ||
+          (x1[k[i][0]] == z2 && x1[k[i][2]] == z2) ||
+          (x1[k[i][2]] == z2 && x1[k[i][1]] == z2)
+        ) {
+          for (let j = 0; j < 3; j++) {
+            if (x1[k[i][j]] != z1) {
+              if (x1[k[i][j]] != z2) {
+                index = k[i][j];
+                console.log(index, "ifff");
+              }
+            }
+          }
+          break;
+        } else if (
+          (x1[k[i][0]] == z1 && x1[k[i][1]] == z1) ||
+          (x1[k[i][0]] == z1 && x1[k[i][2]] == z1) ||
+          (x1[k[i][2]] == z1 && x1[k[i][1]] == z1)
+        ) {
+          for (let j = 0; j < 3; j++) {
+            if (x1[k[i][j]] != z2) {
+              if (x1[k[i][j]] != z1) {
+                index = k[i][j];
+                console.log(index, "eellllifff");
+              }
+            }
+          }
+          break;
+        }
       }
-      setCputurn(false)
-    },500)
-  },[cpu])
+      console.log(index, "___________________________%%%%");
+      x1[index] = choice[0] != "" ? choice[0] : x1[index];
+
+      setValue(x1);
+
+      if (start) {
+        setChoice([z1, false]);
+        if (checkWin(x1)) {
+          setWin(true);
+        }
+      }
+      setCputurn(false);
+    }, 500);
+  }, [cpu]);
 
   return (
     <div className="App">
@@ -72,14 +142,14 @@ function App() {
         board={value}
         playerip={handleplayerip}
         choiceplayer={choice[0]}
-        won = {win}
-        cpu = {cputurn}
+        won={win}
+        cpu={cputurn}
       ></Board>
       <br></br>
       {choice[1] && <div>please choose X|O </div>}
       {choice[1] && <Choice click={handleChoice}></Choice>}
       <br></br>
-      {(win==true)&&<div>{(choice[0]=='X')?'O':'X'} won</div>}
+      {win == true && <div>{choice[0] == "X" ? "O" : "X"} won</div>}
       {/* {!choice[1] && <div>your choice: {choice} start the game</div>} */}
     </div>
   );
